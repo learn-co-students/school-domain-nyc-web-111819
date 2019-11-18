469cdee9b95b04192658dc35fce54f849a54baf4
Alex Williams <alex_williams@protonmail.com>
2019-11-18 18:11:00 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+    def initialize(nameS)
+        @name = nameS
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+    def grade(gradeLevel)
+        @roster[gradeLevel]
+    end
+    def sort
+        for x in @roster.keys
+            @roster[x].sort_by!{ |string| string[0]}
+        end
+        @roster
+    end
+    def add_student(name, grade)
+        if !roster.keys.include? grade
+            roster[grade] = [name]
+        else
+            roster[grade] << name
+        end
+    end
+end
